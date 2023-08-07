c16d45f1478057dd67c3c4b2e24a48f8cfdae6a6
tuannam <namntt@digi-texx.vn>
2023-08-07 10:58:12 +0700

    feature(system-core): pagination

--- src/Pagination/PaginationItem.tsx
+++ src/Pagination/PaginationItem.tsx
@@ -1,18 +1,22 @@
+import { forwardRef } from 'react';
 import { Pagination } from 'react-bootstrap';
+
 import { PageItemProps } from './model';
 
-const PaginationItem = ({ page, key, active, onClick }: PageItemProps) => {
-  const handleChangePage = (page: number) => onClick?.(page);
+const PaginationItem = forwardRef<HTMLDivElement, PageItemProps>(
+  ({ page, key, active, onClick, ...restProps }, ref) => {
+    const handleChangePage = (page: number) => onClick?.(page);
 
-  return (
-    <Pagination.Item
-      key={key}
-      active={active}
-      onClick={() => handleChangePage(page)}
-    >
-      {page}
-    </Pagination.Item>
-  );
-};
+    return (
+      <Pagination.Item
+        key={key}
+        active={active}
+        onClick={() => handleChangePage(page)}
+      >
+        {page}
+      </Pagination.Item>
+    );
+  }
+);
 
 export default PaginationItem;
